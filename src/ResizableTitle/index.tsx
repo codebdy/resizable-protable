bf5ac3bbc88858dfe6c84af2a77508968b0e3278
Water.Li <lizongbin@hotmail.com>
2023-08-13 11:49:00 +0800

    backup

--- src/ResizableTitle/index.tsx
+++ src/ResizableTitle/index.tsx
@@ -1,21 +1,25 @@
 import { ProColumns } from "@ant-design/pro-components"
-import { CSSProperties, memo, useRef, useState } from "react"
+import { memo, useRef, useState } from "react"
+import "./style.css"
 
 export type ResizableColumn<T = any, ValueType = "text"> = ProColumns<T, ValueType> & {
   resiable?: boolean,
 }
 
 export const ResizableTitle = memo((props: {
-  style?: CSSProperties
+  className?: string
   onResize?: (width?: number) => void,
   children?: React.ReactNode,
 }) => {
-  const { style, onResize, children, ...other } = props;
+  const { className, onResize, children, ...other } = props;
   const [width, setWidth] = useState<number>()
   const ref = useRef<HTMLTableCellElement>(null)
   return (
-    <th ref={ref} style={{ ...style || {}, position: "relative" }} {...other}>
+    <th ref={ref} className={"resizable-title" + (className ? (" " + className) : "")} {...other}>
       {children}
+      <div
+        className="resizable-title-handler"
+      />
     </th>
   )
 })
