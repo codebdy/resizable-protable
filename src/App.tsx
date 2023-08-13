ecd82a871557e2195172d998ede72fdf870d6dee
Water.Li <lizongbin@hotmail.com>
2023-08-13 14:08:00 +0800

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Example } from './Example';
+import { ExamplePro } from './ExamplePro';
 
 function App() {
   return (
     <div className="App">
       <Example />
+      <ExamplePro />
     </div>
   );
 }
