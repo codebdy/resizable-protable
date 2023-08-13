e45a1c94f0a7e2ca165d669b60e320eb9b9b1c2d
Water.Li <lizongbin@hotmail.com>
2023-08-13 12:38:47 +0800

    Update index.tsx

--- src/ResizableTitle/index.tsx
+++ src/ResizableTitle/index.tsx
@@ -17,14 +17,20 @@ export const ResizableTitle = memo((props: {
   children?: React.ReactNode,
 }) => {
   const { className, onResize, children, ...other } = props;
+  //上一次鼠标x坐标位置
   const [lastX, setLastX] = useState<number>();
+  //开始信息
   const [startInfo, setStarInfo] = useState<StartInfo>();
+  //把手引用
   const ref = useRef<HTMLTableCellElement>(null)
+  //上一次鼠标x位置医用
   const lastXRef = useRef(lastX)
   lastXRef.current = lastX
+  //开始信息引用
   const startInfoRef = useRef(startInfo)
   startInfoRef.current = startInfo
 
+  //鼠标移动
   const handleMouseMove = useCallback((e: MouseEvent) => {
     const start = startInfoRef.current
     if (start?.width && e.clientX !== lastXRef.current) {
@@ -38,10 +44,12 @@ export const ResizableTitle = memo((props: {
     }
   }, [onResize])
 
+  //鼠标抬起
   const handleMouseUp = useCallback(() => {
     setStarInfo(undefined)
   }, [])
 
+  //鼠标按下
   const handleMousDown = useCallback((e: React.MouseEvent) => {
     setLastX(e.clientX)
     setStarInfo({
